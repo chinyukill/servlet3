0cb0071b6922d4864a6f8e451a2ea036ca5f7d4f
chenxue <1170558396@qq.com>
2019-10-22 22:17:06 +0800

    json

---
+++ src/com/chenxue/bean/Dept.java
@@ -0,0 +1,25 @@
+package com.chenxue.bean;
+
+public class Dept {
+	private int deptNo;
+	private String dName;
+	private String loc;
+	public int getDeptNo() {
+		return deptNo;
+	}
+	public void setDeptNo(int deptNo) {
+		this.deptNo = deptNo;
+	}
+	public String getdName() {
+		return dName;
+	}
+	public void setdName(String dName) {
+		this.dName = dName;
+	}
+	public String getLoc() {
+		return loc;
+	}
+	public void setLoc(String loc) {
+		this.loc = loc;
+	}
+}
